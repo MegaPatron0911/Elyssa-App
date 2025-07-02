8ea0cc29aeb71d959431393102fad7e5e29a9681
MegaPatron0911 <oswaldopatron0911@gmail.com>
2025-07-02 18:41:31 -0500

    Ultimo commit

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8237df33ab2bf010efa1c3fb5bc002b028290402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df21abc0e5b4fedddbcc9d47eb5e2ce7b83a785c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
